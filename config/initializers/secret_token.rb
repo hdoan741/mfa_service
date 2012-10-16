bcaf49e2a0f3085f9ff21f6cd3b70994861c57ed
Hung Doan <hdoan741@gmail.com>
2012-10-17 00:09:33 +0800

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MfaService::Application.config.secret_token = '7ff865be16b5b630fcd0c0113fef8733e909bb419b2be7d4542afe0af73a3384058df689754b902abf36b17725b3350d3817372cfdb4a6e8bad8d2c59d3c255c'
