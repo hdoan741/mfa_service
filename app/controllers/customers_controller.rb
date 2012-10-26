c664d0281a4fc44aef365c972ad7171f866ef4bb
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-10-26 15:27:04 +0800

    Fix email

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -48,7 +48,7 @@ class CustomersController < ApplicationController
       # invalidate the request
       @customer = nil
     end
-    
+
   end
 
   # POST /customers
@@ -82,7 +82,6 @@ class CustomersController < ApplicationController
       if @account_created 
         # Tell the UserMailer to send a welcome Email after save
         @user = @customer
-        UserMailer.welcome_email(@user).deliver
       else
         # Nothing
       end
@@ -97,7 +96,8 @@ class CustomersController < ApplicationController
         
         # Tell the User mailer to send a add company account email
         #@user = @customer2
-        #UserMailer.welcome_add_company_account_email(@user).deliver
+        UserMailer.welcome_add_company_account_email(@user).deliver
+        # UserMailer.welcome_email(@user).deliver
 
         format.html { redirect_to @customer_company, notice: 'Customer Company was successfully created.' }
         format.json { render json: @customer_company, status: :created}
